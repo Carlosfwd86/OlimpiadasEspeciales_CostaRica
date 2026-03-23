4bc577b4a37ca7b93071c7417cbd8e444d2b95ec
Gerson Focus delantero <gfuentesfwd@gmail.com>
2026-03-23 11:11:09 -0600

    agregue informacion en contacto

--- Olimpiadas_Especiales/src/components/Navbar.jsx
+++ Olimpiadas_Especiales/src/components/Navbar.jsx
@@ -25,10 +25,11 @@ const Navbar = () => {
 
       <div className="navbar_menu_derecha">
         <ul className="lista_navegacion">
+          <li className="enlace_nav" onClick={irAlInicio}>INICIO</li>
           <li className="enlace_nav" onClick={irANosotros}>NOSOTROS</li>
           <li className="enlace_nav" onClick={irAEventos}>EVENTOS</li>
           <li className="enlace_nav" onClick={irAVoluntarios}>VOLUNTARIOS</li>
-          <li className="enlace_nav" onClick={irAContacto}>CONTACTO</li>
+          <li className="enlace_nav" onClick={irAContacto}>CONTÁCTANOS</li>
         </ul>
         
         <button className="boton_accion_rojo" onClick={irARegistro}>
