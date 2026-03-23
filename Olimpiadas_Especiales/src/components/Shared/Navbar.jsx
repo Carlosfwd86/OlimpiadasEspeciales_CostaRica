dc7ab397612e1332513cba4d67584c8ba5708a6a
Carlosjr86 <cjimenezrfwd@gmail.com>
2026-03-22 23:14:12 -0600

    Actualización domingo

--- Olimpiadas_Especiales/src/components/Shared/Navbar.jsx
+++ Olimpiadas_Especiales/src/components/Shared/Navbar.jsx
@@ -1,7 +1,7 @@
 /* --- COMPONENTE NAVBAR ESTÉTICO --- */
 
 import React from 'react';
-import "../styles/Navbar.css"
+import "../../styles/Navbar.css"
 
 const Navbar = () => {
 
--- Olimpiadas_Especiales/src/pages/HomePages.jsx
+++ Olimpiadas_Especiales/src/pages/HomePages.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Shared/Navbar';
 import Home from '../components/Home';
 
 
